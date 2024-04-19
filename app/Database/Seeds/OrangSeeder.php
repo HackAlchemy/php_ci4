8b05a0fc79bdbf6e902dc086ce17c7629fe52353
alhammanazil <107827210+Alhammanazil@users.noreply.github.com>
2024-04-19 08:47:16 +0700

    database migration dan database seeding

---
+++ app/Database/Seeds/OrangSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+use CodeIgniter\I18n\Time;
+
+class OrangSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            'nama'          => 'Harry McLaen',
+            'alamat'        => 'Apocalypse Road',
+            'created_at'    => Time::now(),
+            'updated_at'    => Time::now()
+        ];
+
+        // Simple Queries
+        // $this->db->query(
+        //     "INSERT INTO orang (nama, alamat, created_at, updated_at) VALUES(:nama:, :alamat:, :created_at:, :updated_at:)",
+        //     $data
+        // );
+
+        // Using Query Builder
+        $this->db->table('orang')->insert($data);
+    }
+}
